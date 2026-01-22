946453d9845fc6ee02b1bca28ba83bd3d562db07
rubiales <jpse110507@gmail.com>
2026-01-22 09:25:38 +0100

    22/01/2026

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PrueabsPOO;
+
+class Persona
+{
+  private string _nombre;
+
+  public string Nombre
+  {
+    get => _nombre.ToLower();
+    set => _nombre = (value == null) ? "Anónimo" : value;
+  }
+  public Persona(string nombre = "Anónimo")
+  {
+  //  Nombre = nombre;    // -> Pasa por la propiedad, está controlado
+      _nombre = nombre;   // -> Pasa por el campo (_), no está controlado, tira Excepción
+  }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        var pepe = new Persona(null);
+        Console.WriteLine(pepe.Nombre);
+    }
+}
